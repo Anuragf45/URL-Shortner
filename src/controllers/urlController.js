337afdd57d9c3fff30365542e4d295d5d4ea4944
Richard-vinu <richardnova1999@gmail.com>
2022-09-29 12:35:45 +0530

    complete

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -6,26 +6,26 @@ let baseUrl = "http://localhost:3000"
 const shortUrl = async(req,res)=>{
 
 try {
+
+    const data = req.body
+
+
+    let urlcode= shortid.generate() 
+      data.urlCode = urlcode
+  
+      let shortUrl = baseUrl + '/'+ urlcode
+      data.shortUrl = shortUrl
+  
+    let result = await urlModel.create(data)
+  
+    let finaldata = await urlModel.findById(result._id).select({createdAt:0,updatedAt:0,__v:0})
+   
+     res.status(201).send({status:true,data:finaldata})
     
 } catch (error) {
     res.status(500).send({status:false,message:error.message})
 }
-   const data = req.body
 
-
-  let urlcode= shortid.generate() 
-    data.urlCode = urlcode
-
-    let shortUrl = baseUrl + '/'+ urlcode
-    data.shortUrl = shortUrl
-
-  let result = await urlModel.create(data)
-
-  let finaldata = await urlModel.findById(result._id).select({createdAt:0,updatedAt:0,__v:0})
- 
-
-//   console.log(finaldata);
-   res.status(201).send({status:true,data:finaldata})
 
 }
 
